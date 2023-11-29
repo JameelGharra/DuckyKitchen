24792f07df9c998ed5c321ce4e6ae278b7f7f54d
JameelGharra <jameelgharra@gmail.com>
2023-11-29 14:24:11 +0200

    Refactoring the logic for movement and collision.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,53 +6,48 @@ using UnityEngine;
 public class Player : MonoBehaviour { 
     [SerializeField] private float moveSpeed = 20f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private float playerHeight = 2f;
+    [SerializeField] private float playerRadius = 0.57f;
+
     private bool isWalking;
+
+    private bool IsValidMoveDirection(Vector3 moveDirection, float moveDistance) {
+        // Capsule cast requires the top and the bottom of the virtual capsule fired
+        return !Physics.CapsuleCast(transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDirection,
+            moveDistance
+            );
+    }
+    private Vector3 getValidMoveDirection(Vector3 moveDirection, float moveDistance) {
+        if(IsValidMoveDirection(moveDirection, moveDistance)) {
+            return moveDirection;
+        }
+        // Attempt to move towards either X or Z if we cannot move on 
+        Vector3 moveDirX = new Vector3(moveDirection.x, 0, 0);
+        if (IsValidMoveDirection(moveDirX, moveDistance))
+            return moveDirX;
+        // Attempt only Z movement
+        Vector3 moveDirZ = new Vector3(0, 0, moveDirection.z);
+        if (IsValidMoveDirection(moveDirZ, moveDistance))
+            return moveDirZ;
+
+        return Vector3.zero;
+    }
 
     private void Update() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 intendedDirection = new(inputVector.x, 0f, inputVector.y);
 
-        float playerRadius = 0.57f, playerHeight = 2f, moveDistance = moveSpeed * Time.deltaTime;
-        // Capsule cast requires the top and the bottom of the virtual capsule fired
-        bool canMove = !Physics.CapsuleCast(transform.position, 
-            transform.position+ Vector3.up*playerHeight, 
-            playerRadius, 
-            moveDir, 
-            moveDistance
-        );
-        if(!canMove) {
-            // Attempt to move towards either X or Z if we cannot move on both
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
-            canMove = !Physics.CapsuleCast(transform.position,
-            transform.position + Vector3.up * playerHeight,
-            playerRadius,
-            moveDirX,
-            moveDistance
-            );
-            if (canMove) {
-                // Can move only on X
-                moveDir = moveDirX;
-            }
-            else {
-                // Attempt only Z movement
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
-                canMove = !Physics.CapsuleCast(transform.position,
-                transform.position + Vector3.up * playerHeight,
-                playerRadius,
-                moveDirZ,
-                moveDistance
-                );
-                if(canMove) {
-                    moveDir = moveDirZ;
-                }
-            }
+        float moveDistance = moveSpeed * Time.deltaTime;
+        Vector3 moveDirection = getValidMoveDirection(intendedDirection, moveDistance);
+        if (moveDirection != Vector3.zero) {
+            transform.position += moveSpeed * Time.deltaTime * moveDirection;
         }
-        if (canMove) {
-            transform.position += moveDir * Time.deltaTime * moveSpeed;
-        }
-        isWalking = moveDir != Vector3.zero;
+        isWalking = moveDirection != Vector3.zero;
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime*rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime*rotateSpeed);
     }
     public bool IsWalking() {
         return isWalking;
