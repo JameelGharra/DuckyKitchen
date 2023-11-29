ddff15ab9a9169462ea98000742bf2d6ca18a2e1
JameelGharra <jameelgharra@gmail.com>
2023-11-29 13:55:05 +0200

    Improved collision detection by using capsules, made movement possible towards X or Z if direction on X and Z wasn't possible.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,9 +12,41 @@ public class Player : MonoBehaviour {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = 0.7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
-
+        float playerRadius = 0.57f, playerHeight = 2f, moveDistance = moveSpeed * Time.deltaTime;
+        // Capsule cast requires the top and the bottom of the virtual capsule fired
+        bool canMove = !Physics.CapsuleCast(transform.position, 
+            transform.position+ Vector3.up*playerHeight, 
+            playerRadius, 
+            moveDir, 
+            moveDistance
+        );
+        if(!canMove) {
+            // Attempt to move towards either X or Z if we cannot move on both
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDirX,
+            moveDistance
+            );
+            if (canMove) {
+                // Can move only on X
+                moveDir = moveDirX;
+            }
+            else {
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirZ,
+                moveDistance
+                );
+                if(canMove) {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
         if (canMove) {
             transform.position += moveDir * Time.deltaTime * moveSpeed;
         }
