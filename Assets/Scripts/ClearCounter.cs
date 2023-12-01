2e7ae2a0186b8d03fbe792164d622006b03352a2
JameelGharra <jameelgharra@gmail.com>
2023-12-01 12:59:44 +0200

    Added the proper logic for interaction with counters correspanding to their type, added prefab variants for counters.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,6 +7,11 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        
+        if(!HasKitchenObject() && player.HasKitchenObject()) {
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+        }
+        else if(HasKitchenObject() && !player.HasKitchenObject()) {
+            GetKitchenObject().SetKitchenObjectParent(player);
+        }
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -8,8 +8,10 @@ public class ContainerCounter : BaseCounter, IKitchenObjectParent {
 
     public event EventHandler OnPlayerGrabbedObject;
     public override void Interact(Player player) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        if (!player.HasKitchenObject()) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty); // for container counter animation
+        }
     }
 }
