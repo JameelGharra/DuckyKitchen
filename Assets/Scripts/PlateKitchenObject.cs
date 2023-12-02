2d9e54cd8dab457da697d0b9e36c48310a769ab5
JameelGharra <jameelgharra@gmail.com>
2023-12-02 11:42:51 +0200

    Added visuals for ingredients on a plate on process of making a hamburger.

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+    
     private List<KitchenObjectSO> kitchenObjectSOList;
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
     private void Awake() {
         kitchenObjectSOList = new List<KitchenObjectSO>();
     }
@@ -12,6 +19,9 @@ public class PlateKitchenObject : KitchenObject {
         if(kitchenObjectSOList.Contains(kitchenObjectSO) || !validKitchenObjectSOList.Contains(kitchenObjectSO))
             return false;
         kitchenObjectSOList.Add(kitchenObjectSO);
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { 
+            kitchenObjectSO = kitchenObjectSO 
+        });
         return true;
-    } 
+    }
 }
